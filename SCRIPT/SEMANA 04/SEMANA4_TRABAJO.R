14b1719821d8607b472e516ddf8e17edd15475a3
liessyromanlazo <LIESSY.ROMAN@UPSJB.EDU.PE>
2025-12-07 15:24:28 -0500

    Add files via upload

---
+++ SCRIPT/SEMANA 04/SEMANA4_TRABAJO.R
@@ -0,0 +1,6 @@
+
+#ACTIVIDAD: Como descargar un archivo en formato CSV del repositorio GIBHUB
+1. Entramos al repositorio en GitHub donde está el archivo y hacemos 
+   clic sobre el nombre del archivo s12_hipert_covid.csv
+2. En el botón “Download raw file”, le damos Guardar y listo, ya 
+  tenemos el archivo CSV descargado.
